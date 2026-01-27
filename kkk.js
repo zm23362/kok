5b2bd93c3d8ef76e50136824cbfaf2a2b59c68b5
zm23362 <zm23362@josai.ac.jp>
2026-01-28 01:40:06 +0900

    Update kkk.js

--- kkk.js
+++ kkk.js
@@ -1,3 +1,11 @@
+// 全ての画像を取得
 const images = document.querySelectorAll('#slideshow img'); let currentIndex = 0;
-function showNextImage() { images[currentIndex].classList.remove('active'); currentIndex = (currentIndex + 1) % images.length; images[currentIndex].classList.add('active');}
+function showNextImage() {// 現在表示されている画像から active クラスを消す 
+  images[currentIndex].classList.remove('active');
+ // 次の画像の番号を計算（最後の次は最初に戻る）
+  currentIndex = (currentIndex + 1) % images.length;
+ // 次の画像に active クラスをつける 
+  images[currentIndex].classList.add('active');
+}
+// 3000ミリ秒（3秒）ごとに showNextImage 関数を実行 
 setInterval(showNextImage, 3000);
