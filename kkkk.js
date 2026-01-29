5161cb8891ee7d33644a73ae26d7cab9926a0403
zm23362 <zm23362@josai.ac.jp>
2026-01-30 02:24:19 +0900

    Update kkkk.js

--- kkkk.js
+++ kkkk.js
@@ -1,8 +1,1 @@
-function hyoji17(){ var str17 = "吉田綾乃クリスティー、増田三莉音、黒見明香";
-var cnt17 = document.timer17.moji17.value.length;
-if (cnt17 < 22) { // 1文字ずつ増やしていく
-document.timer17.moji17.value = str17.substr(0, cnt17 + 1);
-} else { // 最後まで行ったら空文字に戻す
- document.timer17.moji17.value = ""; }
-// ifの外に書くことで、増殖中もリセット後も必ず1秒後にまた実行される
- setTimeout(hyoji17, 1000); }
+
