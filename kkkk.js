2ae0b06d1ccc93d2bd0bacc69800fc369663647f
zm23362 <zm23362@josai.ac.jp>
2026-01-30 02:18:05 +0900

    Update kkkk.js

--- kkkk.js
+++ kkkk.js
@@ -6,8 +6,8 @@ var cnt17= document.timer17.moji17.value.length;
 //文字が全部表示されているか確認
 if (cnt17 < 22 ) {
 //現在より1文字多く切り出して表示
-document.timer17.moji17.value =str17.substr(0,cnt17+1); } else {
-//全て表示されたら、空文字に戻す
-document.timer17.moji17.value = ""; }
+document.timer17.moji17.value =str17.substr(0,cnt17+1); } 
+else {//全て表示されたら、空文字に戻す
+  document.timer17.moji17.value = ""; }
 //setTimeout()を含む関数を呼び出す
 setTimeout("hyoji17()",1000); }
