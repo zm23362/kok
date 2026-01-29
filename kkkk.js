28d6bd383c5c90d76d5c2d03447838629f0743d8
zm23362 <zm23362@josai.ac.jp>
2026-01-30 02:49:18 +0900

    Update kkkk.js

--- kkkk.js
+++ kkkk.js
@@ -1,19 +1,13 @@
 function hyoji17(){
-    // 表示する文字列
-    var str17 = "吉田綾乃クリスティー、増田三莉音、黒見明香";
-    
-    // フォームの要素を変数に入れて、エラーを防ぐ
-    var target = document.timer17.moji17;
-    var cnt17 = target.value.length;
-
-    // 全22文字。もし22文字以上（最後まで）表示されていたらリセット
-    if (cnt17 >= 22) {
-        target.value = ""; // 一旦空にする
-    } else {
-        // 1文字増やす
-        target.value = str17.substr(0, cnt17 + 1);
-    }
-
-    // 文字列としてではなく、関数名として呼び出す（これが最も重要です）
-    setTimeout(hyoji17, 1000);
-}
+//表示する文字
+var str17= "乃木坂46";
+//テキストボックスの文字数
+var cnt17= document.timer17.moji17.value.length;
+//文字が全部表示されているか確認
+if (cnt17 < 5 ) {
+//現在より1文字多く切り出して表示
+document.timer17.moji17.value =str17.substr(0,cnt17+1); } else {
+//全て表示されたら、空文字に戻す
+document.timer17.moji17.value = ""; }
+//setTimeout()を含む関数を呼び出す
+setTimeout("hyoji17()",1000); }
